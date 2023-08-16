53be62daefb882ec98f7129dceaa4529dcde20c4
Changwoo Nam <whoshe@funnyplan.com>
2023-08-16 18:16:45 +0900

    second commit

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,8 @@
-import '@/styles/globals.css'
+import '@/styles/globals.css';
+import {Amplify} from 'aws-amplify';
+import awsConfigure from '@/aws-exports';
+
+Amplify.configure(awsConfigure);
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />
